ea027eaa9c56c906dda1536646bac1776c821233
Leo Cornelius <kegcoingold@gmail.com>
2019-06-14 17:41:51 +0100

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -17,8 +17,4 @@ const std::string asciiArt =
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
    the art itself, and remove these two #ifdefs and above ascii arts */
-#ifdef _WIN32
-const std::string asciiArt = windowsAsciiArt;
-#else
-const std::string asciiArt = nonWindowsAsciiArt;
-#endif
+
