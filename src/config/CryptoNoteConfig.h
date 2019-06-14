71d5f1e9221411257f6dbca150ac8a8a9ec7ecd3
Leo Cornelius <kegcoingold@gmail.com>
2019-06-14 17:28:54 +0100

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -18,11 +18,11 @@
 namespace CryptoNote {
 namespace parameters {
 
-const uint64_t DIFFICULTY_TARGET                             = 15; // seconds
+const uint64_t DIFFICULTY_TARGET                             = 1800; // 1800 seconds (30 mins)
 
-const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000000;
-const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
-const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 10000000;
+const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = (uint32_t)(-1);
+const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = (size_t)(-1);
+const size_t   CRYPTONOTE_MAX_TX_SIZE                        = (size_t)(-1);
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 3121939;
 const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 5;
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
@@ -154,9 +154,10 @@ static_assert(0 < UPGRADE_VOTING_THRESHOLD && UPGRADE_VOTING_THRESHOLD <= 100, "
 static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 
 /* Block heights we are going to have hard forks at */
-const uint64_t FORK_HEIGHTS[] =
-{  1,
-   2,}; // place holders
+const uint64_t FORK_HEIGHTS[] =  {
+  1,
+  2,
+}; // place holders
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
 const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX                 = 1;
 
